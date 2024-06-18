aafc06162dbf4e81c84cd4a90c70aa79f8049af5
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-18 14:03:42 +0700

    Add views

--- quickstart_app/views.py
+++ quickstart_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import GroupSerializer
+from .serializers import UserSerializer
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
